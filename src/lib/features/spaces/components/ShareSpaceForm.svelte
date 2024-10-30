682bef7529564bb4df1e2ff9e5ffd392214c396a
MadeInPierre <pielaclau@gmail.com>
2024-10-30 14:48:33 +0100

    feat: simple space members settings page

---
+++ src/lib/features/spaces/components/ShareSpaceForm.svelte
@@ -0,0 +1,63 @@
+<script lang="ts">
+	import { Button } from '$lib/shared/components/ui/button';
+	import * as Dialog from '$lib/shared/components/ui/dialog/index.js';
+	import { Input } from '$lib/shared/components/ui/input';
+	import { Label } from '$lib/shared/components/ui/label';
+	import { siteConfig } from '$lib/shared/config/site-config';
+	import { Check, Copy } from 'lucide-svelte';
+	import { copyText } from 'svelte-copy';
+	import { toast } from 'svelte-sonner';
+	import { getActiveSpaceState } from '../state/active-space.svelte';
+
+	let copied = $state(false);
+
+	const activeSpace = getActiveSpaceState();
+
+	const inviteUrl = $derived(siteConfig.inviteUrlBase + activeSpace.id);
+
+	function copyLink() {
+		copyText(
+			`Hey! Let's share a Cuicuit space. It's super easy, just join using this link: ${inviteUrl}`
+		);
+		copied = true;
+		toast.success('Link copied to clipboard', {
+			description: 'Groceries will be fun again! 🎉'
+		});
+		setTimeout(() => {
+			copied = false;
+		}, 3000);
+	}
+
+	// const inviteUrl = `https://cuicuit.fr/go/${generateUniqueId()}`;
+	// function generateUniqueId() {
+	// 	const timestamp = Date.now().toString(36).substring(3);
+	// 	const randomString = Math.random().toString(36).substring(2, 7);
+	// 	return timestamp + randomString;
+	// }
+</script>
+
+<div class="grid gap-4 py-4">
+	<div class="space-y-2">
+		<Label for="name">Invite people</Label>
+
+		<div class="flex w-full gap-2">
+			<Input id="name" value={inviteUrl} readonly tabindex={-1} />
+
+			<Button on:click={copyLink}>
+				{#if copied}
+					<div class="flex items-center gap-2">
+						Copied!
+						<Check class="size-4" />
+					</div>
+				{:else}
+					<div class="flex items-center gap-2">
+						Copy
+						<Copy class="size-4" />
+					</div>
+				{/if}
+			</Button>
+		</div>
+
+		<p class="text-xs text-muted-foreground">Share this link with your friends to invite them!</p>
+	</div>
+</div>
--- src/routes/(app)/settings/space/members/+page.svelte
+++ src/routes/(app)/settings/space/members/+page.svelte
@@ -1,1 +1,61 @@
-TODO
+<script lang="ts">
+	import { getActiveSpaceState } from '$lib/features/spaces/state/active-space.svelte';
+	import { Button } from '$lib/shared/components/ui/button';
+	import { Separator } from '$lib/shared/components/ui/separator';
+	import { getUserDocState } from '$lib/features/auth/state/user-doc-state.svelte';
+	import UserAvatar from '$lib/features/user-settings/components/UserAvatar.svelte';
+	import ShareSpaceForm from '$lib/features/spaces/components/ShareSpaceForm.svelte';
+	import { Star, LogOut } from 'lucide-svelte';
+	import { toast } from 'svelte-sonner';
+
+	const activeSpace = getActiveSpaceState();
+
+	async function removeMember(uid: string) {
+		toast.error('Not implemented');
+		throw new Error('Not implemented');
+	}
+</script>
+
+<div class="space-y-6">
+	<div>
+		<h3 class="text-lg font-medium">{activeSpace.doc?.name} members</h3>
+		<p class="text-sm text-muted-foreground">Manage the members of the currently active space.</p>
+	</div>
+	<Separator />
+
+	<ShareSpaceForm />
+
+	<legend class="text-lg font-medium">Members</legend>
+
+	<div class="grid gap-4 grid-cols-2">
+		<div class="border rounded-lg p-3 flex items-center gap-4 shadow-sm">
+			{#each Object.entries(activeSpace.doc?.memberProfiles || {}) as [memderUid, profile] (memderUid)}
+				<UserAvatar {profile} class="size-12" />
+				<div class="grid">
+					<div class="flex items-center text-sm font-medium">
+						{profile.firstName}
+						{profile.lastName}
+
+						<div
+							class="ml-2 flex items-center text-yellow-500 text-xs gap-1 rounded-full border-yellow-500 border-2 px-2 py-0.5"
+						>
+							<Star class="size-3.5" fill="#eab308" />
+							Admin
+						</div>
+					</div>
+					<p class="text-sm text-muted-foreground">@{profile.userName}</p>
+				</div>
+				<div class="ml-auto mr-2">
+					<Button
+						size="icon"
+						variant="ghost"
+						class="size-8 hover:bg-red-50 dark:hover:bg-red-950"
+						onclick={() => removeMember(memderUid)}
+					>
+						<LogOut class="size-4 text-red-600" />
+					</Button>
+				</div>
+			{/each}
+		</div>
+	</div>
+</div>
