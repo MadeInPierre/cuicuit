f1934cb23e845ee908b3a187d40bad08ee2c9f99
MadeInPierre <pielaclau@gmail.com>
2024-10-30 14:48:33 +0100

    feat: simple space members settings page

--- src/lib/features/user-settings/components/UserAvatar.svelte
+++ src/lib/features/user-settings/components/UserAvatar.svelte
@@ -2,31 +2,45 @@
 	import * as Avatar from '$lib/shared/components/ui/avatar';
 	import { getUserDocState } from '$lib/features/auth/state/user-doc-state.svelte';
 	import { nature_icons } from '$lib/shared/icons/nature-icons';
+	import type { UserProfile } from '$lib/features/auth/db/user-doc';
 
-	let className: string | undefined | null = undefined;
-	export { className as class };
+	interface Props {
+		class?: string;
+		profile?: UserProfile;
+	}
+
+	// Take any user profile as input or use the current user's profile by default
+	let { class: className, profile = undefined }: Props = $props();
 
 	const userDocState = getUserDocState();
+	const displayProfile = $derived(profile ? profile : userDocState.doc);
+	const UserIcon = $derived(nature_icons[displayProfile?.avatar.icon || 'bird']);
 </script>
 
-{#if userDocState.user && userDocState.doc}
+{#if displayProfile}
 	<Avatar.Root class={className}>
-		{#if userDocState.doc.avatar.type == 'image'}
+		{#if displayProfile.avatar.type == 'image'}
 			<Avatar.Image
-				src={userDocState.doc.avatar.url}
-				alt={userDocState.doc.userName || userDocState.doc.firstName}
+				src={displayProfile.avatar.url}
+				alt={displayProfile.userName || displayProfile.firstName}
 				class="object-cover"
 			/>
 		{/if}
 
-		{#if userDocState.doc.avatar.type == 'icon' && Object.keys(nature_icons).includes(userDocState.doc.avatar.icon)}
+		{#if displayProfile.avatar.type == 'icon' && Object.keys(nature_icons).includes(displayProfile.avatar.icon)}
 			<Avatar.Fallback>
-				<svelte:component this={nature_icons[userDocState.doc.avatar.icon]} class="h-1/2 w-1/2" />
+				<UserIcon class="h-1/2 w-1/2"></UserIcon>
 			</Avatar.Fallback>
 		{:else}
 			<Avatar.Fallback>
-				{userDocState.doc.firstName?.charAt(0) || ''}
+				{displayProfile.firstName?.charAt(0) || ''}
 			</Avatar.Fallback>
 		{/if}
+	</Avatar.Root>
+{:else}
+	<Avatar.Root class={className}>
+		<Avatar.Fallback>
+			<UserIcon class="h-1/2 w-1/2"></UserIcon>
+		</Avatar.Fallback>
 	</Avatar.Root>
 {/if}
