e33f95f1183b8ac96c9a6da677ec55628944941b
MadeInPierre <pielaclau@gmail.com>
2024-10-30 13:12:10 +0100

    build: add bundle size visualizer, see vite.config.ts

--- vite.config.ts
+++ vite.config.ts
@@ -1,8 +1,19 @@
 import { sveltekit } from '@sveltejs/kit/vite';
 import { defineConfig } from 'vitest/config';
+import { visualizer } from 'rollup-plugin-visualizer';
 
 export default defineConfig({
-	plugins: [sveltekit()],
+	plugins: [
+		sveltekit(),
+		visualizer({
+			// Plugin used to generate a visual representation of the production bundle.
+			// It generates an HTML file with a treemap of the bundle size.
+			// Run `npm run build` and open `.svelte-kit/output/client/stats.html` in
+			// your browser to see the treemap.
+			emitFile: true,
+			filename: 'stats.html'
+		})
+	],
 	test: {
 		include: ['src/**/*.{test,spec}.{js,ts}']
 	}
