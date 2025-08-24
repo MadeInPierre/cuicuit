afb2600177aaebddecdb19a8e23b62f9e3a13502
MadeInPierre <pielaclau@gmail.com>
2025-08-24 21:14:52 +0200

    refactor: cleanup, remove supermarket aisle table, store in client instead

--- src/lib/features/recipes/queries/get-supermarket-aisles.ts
+++
@@ -1,5 +0,0 @@
-import { supabase } from '$lib/shared/db/supabase-client';
-
-export function getSupermarketAisles() {
-	return supabase.from('supermarket_aisles').select().order('aisle', { ascending: true });
-}
---
+++ supabase/migrations/20250824182515_drop_table_aisles.sql
@@ -0,0 +1,2 @@
+-- We don't need this table, better just hardcode the values in the client
+drop materialized view supermarket_aisles;
