0f1519134b23be8ba848de0361bd0377bf97d5a3
MadeInPierre <pielaclau@gmail.com>
2026-07-04 16:20:26 +0200

    fix: stuck on welcome

--- src/routes/+layout.server.ts
+++ src/routes/+layout.server.ts
@@ -1,7 +1,12 @@
+import { supabase } from '$lib/shared/db/supabase-client.svelte';
 import type { LayoutServerLoad } from './$types';
 
-export const load: LayoutServerLoad = async ({ cookies }) => {
+export const load: LayoutServerLoad = async ({ cookies, locals: { supabase: sb } }) => {
+	// Make the supabase client (server or brower) available globally
+	supabase.client = sb;
+
+	// Give +layout.ts the cookies to setup the supabase server client
 	return {
-		cookies: cookies.getAll(),
+		cookies: cookies.getAll()
 	};
 };
