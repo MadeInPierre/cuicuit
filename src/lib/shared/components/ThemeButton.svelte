954dc914a05d6fd7b7fe2e3f6225e7632e628f1d
--global <pielaclau@gmail.com>
2026-08-24 19:08:04 +0200

    fix(sidebar): add collapsible button, fix theme button & meal card grad
    closes #49

--- src/lib/shared/components/ThemeButton.svelte
+++ src/lib/shared/components/ThemeButton.svelte
@@ -1,5 +1,6 @@
 <script lang="ts">
 	import { Button } from '$lib/shared/components/ui/button';
+	import { cn } from '$lib/utils';
 	import { setMode, userPrefersMode } from 'mode-watcher';
 	import { toast } from 'svelte-sonner';
 
@@ -17,39 +18,46 @@
 	}
 
 	interface Props {
+		label?: string;
 		class?: string;
 	}
 
-	const { class: className }: Props = $props();
+	const { label = undefined, class: className }: Props = $props();
 </script>
 
-<Button
-	onclick={() => setMode($userPrefersMode === 'dark' ? 'light' : 'dark')}
-	variant="ghost"
-	size="icon-sm"
-	class={className}
->
-	<svg
-		xmlns="http://www.w3.org/2000/svg"
-		width="24"
-		height="24"
-		viewBox="0 0 24 24"
-		fill="none"
-		stroke="currentColor"
-		stroke-width="2"
-		stroke-linecap="round"
-		stroke-linejoin="round"
-		class="size-4.5"
+<div>
+	<Button
+		onclick={() => setMode($userPrefersMode === 'dark' ? 'light' : 'dark')}
+		variant="ghost"
+		size="icon-sm"
+		class={cn('block w-full justify-start font-normal', className)}
 	>
-		<path stroke="none" d="M0 0h24v24H0z" fill="none" />
-		<path d="M12 12m-9 0a9 9 0 1 0 18 0a9 9 0 1 0 -18 0" />
-		<path d="M12 3l0 18" />
-		<path d="M12 9l4.65 -4.65" />
-		<path d="M12 14.3l7.37 -7.37" />
-		<path d="M12 19.6l8.85 -8.85" />
-	</svg>
-	<span class="sr-only">{$userPrefersMode === 'dark' ? 'Light mode' : 'Dark mode'}</span>
-</Button>
+		<svg
+			xmlns="http://www.w3.org/2000/svg"
+			width="24"
+			height="24"
+			viewBox="0 0 24 24"
+			fill="none"
+			stroke="currentColor"
+			stroke-width="2"
+			stroke-linecap="round"
+			stroke-linejoin="round"
+			class="size-4.5"
+		>
+			<path stroke="none" d="M0 0h24v24H0z" fill="none" />
+			<path d="M12 12m-9 0a9 9 0 1 0 18 0a9 9 0 1 0 -18 0" />
+			<path d="M12 3l0 18" />
+			<path d="M12 9l4.65 -4.65" />
+			<path d="M12 14.3l7.37 -7.37" />
+			<path d="M12 19.6l8.85 -8.85" />
+		</svg>
+		<span class="sr-only">{$userPrefersMode === 'dark' ? 'Light mode' : 'Dark mode'}</span>
+
+		{#if label}
+			<span>{label}</span>
+		{/if}
+	</Button>
+</div>
 
 <!-- {#snippet button(
 	ModeIcon: any,
