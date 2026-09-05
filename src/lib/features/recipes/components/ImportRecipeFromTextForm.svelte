89df017b74c586f633932d8a2554c025de7ba087
--global <pielaclau@gmail.com>
2026-09-05 20:07:13 +0200

    fix(import): import from text input area could get wider than screen width

--- src/lib/features/recipes/components/ImportRecipeFromTextForm.svelte
+++ src/lib/features/recipes/components/ImportRecipeFromTextForm.svelte
@@ -112,7 +112,7 @@
 				id="recipe-text"
 				placeholder="Paste or write a recipe in your own words with ingredients and steps..."
 				bind:value={text}
-				class="min-h-60 max-h-100 min-w-0"
+				class="min-h-30 max-h-30 md:max-h-100 min-w-0"
 				oninput={() => (textError = null)}
 			/>
 
