9b80bc948e7c1197f0ef7114ecc4d0c3d87bfb36
--global <pielaclau@gmail.com>
2026-09-13 16:23:42 +0200

    style: minor UX tweaks

--- src/lib/shared/components/ui/card/card.svelte
+++ src/lib/shared/components/ui/card/card.svelte
@@ -13,7 +13,7 @@
 
 <div
 	bind:this={ref}
-	class={cn("bg-card text-card-foreground rounded-lg border shadow-sm", className)}
+	class={cn("bg-card text-card-foreground rounded-lg shadow-md", className)}
 	{...restProps}
 >
 	{@render children?.()}
